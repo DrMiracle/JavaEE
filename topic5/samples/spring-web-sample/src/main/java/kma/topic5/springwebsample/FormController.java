314868833927a9951f43baeb809441c44de17cd8
DrMiracle <yevgenstefaniuk@gmail.com>
2022-02-14 22:54:21 +0200

    Homework 3

---
+++ topic5/samples/spring-web-sample/src/main/java/kma/topic5/springwebsample/FormController.java
@@ -0,0 +1,55 @@
+package kma.topic5.springwebsample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class FormController {
+
+    List<Book> bookList = new ArrayList<>();
+
+    @RequestMapping(value = "/form", method = RequestMethod.GET)
+    public String formControllerGet() {
+        return "form-controller-get";
+    }
+
+    @RequestMapping(value = "/form", method = RequestMethod.POST)
+    public String formControllerPost(@ModelAttribute FormModel formModel, Model model) {
+        model.addAttribute("formModel", formModel);
+        return "form-controller-post";
+    }
+
+    @GetMapping("/getBooks")
+    public String getAllBooks(Model model){
+        model.addAttribute("books", bookList);
+        return "books";
+    }
+
+    @RequestMapping(value = "/book/add", method = RequestMethod.GET)
+    public String addBookPage(){
+        return "create-book";
+    }
+
+    @RequestMapping(value = "/addBook", method = RequestMethod.POST)
+    public String addBook(
+            @RequestParam(name = "isbn") String isbn,
+            @RequestParam(name = "title") String title,
+            @RequestParam(name = "author") String author, Model model){
+        Book book = new Book();
+        book.setAuthor(author);
+        book.setIsbn(isbn);
+        book.setTitle(title);
+        if (isbn.equals("") || author.equals("")||title.equals("")){
+            model.addAttribute("errorText","Check book data");
+            return "create-book";
+        }
+        if(!bookList.contains(book))
+            bookList.add(book);
+        return "redirect:/getBooks";
+    }
+
+}
---
+++ topic5/samples/spring-web-sample/src/main/java/kma/topic5/springwebsample/FormModel.java
@@ -0,0 +1,15 @@
+package kma.topic5.springwebsample;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class FormModel {
+
+    private String login;
+    private String password;
+
+}
